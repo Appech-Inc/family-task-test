97772b109073e60ebe0cf9ba6b9561507c7a6efa
Johnathon Smith <john.smith@appech.com>
2020-07-31 08:12:23 -0400

    First Checkin
    Projects Created

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -17,7 +17,8 @@ namespace WebClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+                {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
             await builder.Build().RunAsync();
         }
