b317e3a4c83fed06f249cd92087c64d5f1a46649
Sovit P <sovitp@Sovits-MacBook-Pro.local>
2020-11-17 15:14:25 +0545

    Member.cs: added ICollection of Tasks for Member model to add navigational property

--- Domain/DataModels/Member.cs
+++ Domain/DataModels/Member.cs
@@ -12,5 +12,6 @@ namespace Domain.DataModels
         public string Email { get; set; }
         public string Roles { get; set; }
         public string Avatar { get; set; }
+        public ICollection<Task> Tasks { get; set; }
     }
 }
