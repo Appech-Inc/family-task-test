9999535643bbc7694605f12cd8d5993f96f53510
Sovit P <sovitp@Sovits-MacBook-Pro.local>
2020-11-17 15:34:49 +0545

    TaskModel: created functionality to map TaskVm with TaskModel

--- WebClient/Shared/Models/TaskModel.cs
+++ WebClient/Shared/Models/TaskModel.cs
@@ -1,12 +1,33 @@
 using System;
+using Domain.ViewModel;
 
 namespace WebClient.Shared.Models
 {
     public class TaskModel
     {
+        public TaskModel() { }
+
+        public TaskModel(TaskVm taskVm)
+        {
+            this.Id = taskVm.Id;
+            this.IsDone = taskVm.IsComplete;
+            this.Member = taskVm.AssignedMemberId;
+            this.Text = taskVm.Subject;
+        }
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid Member { get; set; }
         public string Text { get; set; }
         public bool IsDone { get; set; }
+
+        public TaskVm ConvertToTaskVm()
+        {
+            return new TaskVm()
+            {
+                Id = this.Id,
+                AssignedMemberId = this.Member,
+                IsComplete = this.IsDone,
+                Subject = this.Text
+            };
+        }
     }
 }
