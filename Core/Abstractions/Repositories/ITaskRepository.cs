a8a9030c6cc233c341e16d84407c46dcf4c43015
Sovit P <sovitp@Sovits-MacBook-Pro.local>
2020-11-17 12:01:39 +0545

    TaskRepository: created an interface and implementation for TaskRepository

---
+++ Core/Abstractions/Repositories/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Abstractions.Repositories;
+
+namespace Core.Abstractions.Repositories
+{
+    public interface ITaskRepository : IBaseRepository<Guid, Task, ITaskRepository>
+    {
+    }
+}
---
+++ DataLayer/Repositories/TaskRepository.cs
@@ -0,0 +1,25 @@
+using Core.Abstractions.Repositories;
+using Domain.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class TaskRepository : BaseRepository<Guid, Task, TaskRepository>, ITaskRepository
+    {
+        public TaskRepository(FamilyTaskContext context) : base(context)
+        { }
+
+        ITaskRepository IBaseRepository<Guid, Task, ITaskRepository>.NoTrack()
+        {
+            return base.NoTrack();
+        }
+
+        ITaskRepository IBaseRepository<Guid, Task, ITaskRepository>.Reset()
+        {
+            return base.Reset();
+        }
+
+    }
+}
