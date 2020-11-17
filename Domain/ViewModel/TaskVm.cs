914ee17e84fa2d670dd852d0b7e7b613852e048d
Sovit P <sovitp@Sovits-MacBook-Pro.local>
2020-11-17 15:15:07 +0545

    TaskVm: created Task view model

---
+++ Domain/ViewModel/TaskVm.cs
@@ -0,0 +1,15 @@
+using Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.ViewModel
+{
+    public class TaskVm
+    {
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public bool IsComplete { get; set; }
+        public Guid AssignedMemberId { get; set; }
+    }
+}
